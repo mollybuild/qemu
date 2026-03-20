dd3854ec8c801acd2e2d1bbbda3d330bfbad1487
MollyChen <xiaoou@iscas.ac.cn>
2026-03-20 08:35:31 +0000

    target/riscv: rvp: Add option defines and dependency check for packed simd extension
    Signed-off-by: Molly Chen <xiaoou@iscas.ac.cn>

--- target/riscv/cpu.h
+++ target/riscv/cpu.h
@@ -70,6 +70,7 @@ typedef struct CPUArchState CPURISCVState;
 #define RVG RV('G')
 #define RVB RV('B')
 #define RVX RV('X')
+#define RVP RV('P')
 
 extern const uint32_t misa_bits[];
 const char *riscv_get_misa_ext_name(uint32_t bit);
--- target/riscv/tcg/tcg-cpu.c
+++ target/riscv/tcg/tcg-cpu.c
@@ -551,6 +551,11 @@ static void riscv_cpu_validate_b(RISCVCPU *cpu)
     }
 }
 
+static void riscv_cpu_validate_p(RISCVCPU *cpu)
+{
+    /* Enable sub-extensions here. Do nothing for now. */
+}
+
 /*
  * Check consistency between chosen extensions while setting
  * cpu->cfg accordingly.
@@ -567,6 +572,10 @@ void riscv_cpu_validate_set_extensions(RISCVCPU *cpu, Error **errp)
 
     if (riscv_has_ext(env, RVB)) {
         riscv_cpu_validate_b(cpu);
+    }
+
+    if (riscv_has_ext(env, RVP)) {
+        riscv_cpu_validate_p(cpu);
     }
 
     if (riscv_has_ext(env, RVI) && riscv_has_ext(env, RVE)) {
@@ -630,6 +639,12 @@ void riscv_cpu_validate_set_extensions(RISCVCPU *cpu, Error **errp)
 
     if (riscv_has_ext(env, RVD) && !riscv_has_ext(env, RVF)) {
         error_setg(errp, "D extension requires F extension");
+        return;
+    }
+
+    if (riscv_has_ext(env, RVP) &&
+        !(cpu->cfg.ext_zba && cpu->cfg.ext_zbb && cpu->cfg.ext_zbkb)) {
+        error_setg(errp, "P extension requires zba, zbb and zbkb extensions");
         return;
     }
 
@@ -1414,6 +1429,7 @@ static const RISCVCPUMisaExtConfig misa_ext_cfgs[] = {
     MISA_CFG(RVV, false),
     MISA_CFG(RVG, false),
     MISA_CFG(RVB, false),
+    MISA_CFG(RVP, false),
 };
 
 /*
